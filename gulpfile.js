f255f69dcd3f21e24b33f63797f2259229fb7ff8
2hdddg <2hdddg@gmail.com>
2014-04-07 21:19:36 +0200

    First test

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var mocha  = require('gulp-mocha');
 
 gulp.task('default', function(){
 });
 
 gulp.task('jshint', function(){
-    gulp.src('./lib/**/*.js')
+    gulp.src(['./lib/**/*.js', './test/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+  return gulp
+    .src('test/*.js')
+    .pipe(mocha());
 });
---
+++ test/agent_unit_test.js
@@ -0,0 +1,47 @@
+var assert = require('assert');
+var sandbox = require('sandboxed-module');
+
+function serviceFake(){
+    var start_calls = 0;
+
+    return {
+        start: function(){
+            start_calls++;
+        },
+
+        // for inspection
+        start_calls: function() {
+            return start_calls;
+        }
+    };
+}
+
+function registrationFake(){
+    return {
+        command: {}
+    };
+}
+
+describe('agent.start()', function(){
+    var Agent = sandbox.require('../lib/agent/agent', {
+        requires: {
+            './service': {
+                create: serviceFake
+            }
+        }
+    });
+
+    it('should start all registered services', function() {
+        var agent = Agent.create({
+            get_registrations: function(callback){
+                callback(null, [registrationFake()]);
+            }
+        })
+
+        agent.start();
+
+        var services = agent.get_services();
+
+        assert.equal(services[0].start_calls(), 1);
+    });
+});
