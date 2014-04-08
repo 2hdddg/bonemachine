5351b4bbb3e16c9660c3613970c5e8c1f240ff84
2hdddg <2hdddg@gmail.com>
2014-04-08 23:20:20 +0200

    Failing test to pick up

--- test/registry_smoke_test.js
+++ test/registry_smoke_test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var _ = require('lodash');
+var fs = require('fs');
 var Registry = require('../lib/agent/registry');
 
 // reads the real thing from filesystem since
@@ -7,6 +8,41 @@ var Registry = require('../lib/agent/registry');
 
 function open_registry(){
     return Registry.open(__dirname + '/registry');
+}
+
+var deleteFolderRecursive = function(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+
+function open_empty_registry(){
+    return Registry.open(__dirname + '/registry_empty');
+}
+
+function ensure_empty_registry_is_empty(){
+    var registry_path = __dirname + '/registry_empty';
+    deleteFolderRecursive(registry_path);
+}
+
+function create_registration(port, name, state){
+    return {
+        port: port,
+        name: name,
+        state: state,
+        start: {
+            command: 'x',
+            args: [1, 2]
+        }
+    };
 }
 
 
@@ -38,6 +74,24 @@ describe('registry.get_registrations', function(){
             })
             assert.strictEqual(on_port_12.name, 'service on port 12');
             done();
+        });
+    });
+});
+
+describe('registry.register', function(){
+    it('can register', function(done){
+        ensure_empty_registry_is_empty();
+        var registry = open_empty_registry();
+        var registration = create_registration(666, 'a name', 'installed');
+        registry.register(registration, function(error, r){
+            // just to check that it adds registration to callback
+            assert.strictEqual(r.port, registration.port);
+            // reopen to ensure that it was persisted
+            registry = open_empty_registry();
+            registry.get_registrations(function(error, registrations){
+                assert.strictEqal(registrations[0].state, 'installed');
+            });
+
         });
     });
 });
